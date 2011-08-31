d74d017231bb618263eb24623f47ff92aabcd0c7
Pieter van der Merwe <pietercvdm@gmail.com>
2011-08-31 00:49:00 +0000

    Updated copyright notice to full name.

--- src/com/debortoliwines/openerp/api/FieldCollection.java
+++ src/com/debortoliwines/openerp/api/FieldCollection.java
@@ -14,7 +14,7 @@
  *   You should have received a copy of the GNU Lesser General Public License
  *   along with OpenERPJavaAPI.  If not, see <http://www.gnu.org/licenses/>.
  *
- *   Copyright 2011 De Bortoli Wines
+ *   Copyright 2011 De Bortoli Wines Pty Limited (Australia)
  */
 
 package com.debortoliwines.openerp.api;
