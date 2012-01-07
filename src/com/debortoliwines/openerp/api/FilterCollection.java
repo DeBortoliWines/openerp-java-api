1ddb087fedaf266134c5d837dfcd6ef9fe5fc861
Pieter van der Merwe <pietercvdm@gmail.com>
2012-01-07 05:48:11 +0000

    1.  Added functionality to add a filter at a specific index. 2.  The size of the filterCollection can now be queried

--- src/com/debortoliwines/openerp/api/FilterCollection.java
+++ src/com/debortoliwines/openerp/api/FilterCollection.java
@@ -55,12 +55,13 @@ public class FilterCollection {
 	
 	/**
 	 * Adds a filter specification to the existing list of filters
+	 * @param index Index where the specified filter should be added
 	 * @param fieldName Name of the model that should be filtered on
 	 * @param comparison For example =, !=, >, >=, <, <=, like, ilike, in, not in, child_of, parent_left, parent_right
 	 * @param value value that will be compared to 'fieldName' using the 'comparison'
 	 * @throws OpeneERPApiException 
 	 */
-	public void add(String fieldName, String comparison, Object value) throws OpeneERPApiException{
+	public void add(int index, String fieldName, String comparison, Object value) throws OpeneERPApiException{
 		if (fieldName == null)
 			throw new OpeneERPApiException("First filter parameter is mandatory.  Please read the OpenERP help.");
 		
@@ -68,7 +69,18 @@ public class FilterCollection {
 			throw new OpeneERPApiException("Second filter parameter is mandatory.  Please read the OpenERP help.");
 		
 		Object [] filter = new Object[] {fieldName,comparison,value};
-		filters.add(filter);
+		filters.add(index, filter);
+	}
+	
+	/**
+	 * Adds a filter specification to the existing list of filters
+	 * @param fieldName Name of the model that should be filtered on
+	 * @param comparison For example =, !=, >, >=, <, <=, like, ilike, in, not in, child_of, parent_left, parent_right
+	 * @param value value that will be compared to 'fieldName' using the 'comparison'
+	 * @throws OpeneERPApiException 
+	 */
+	public void add(String fieldName, String comparison, Object value) throws OpeneERPApiException{
+		add(filters.size(), fieldName, comparison, value);
 	}
 	
 	/**
@@ -89,12 +101,34 @@ public class FilterCollection {
 	 * @param operator
 	 */
 	public void add(FilterOperator operator){
+		add(filters.size(),operator);
+	}
+	
+	/**
+	 * Adds logical operators for filters
+	 *
+	 * From the OpenERP code:
+	 *  Domain criteria can be combined using 3 logical operators than can be added between tuples:  '**&**' (logical AND, default), '**|**' (logical OR), '**!**' (logical NOT).
+     *  These are **prefix** operators and the arity of the '**&**' and '**|**' operator is 2, while the arity of the '**!**' is just 1.
+     *  Be very careful about this when you combine them the first time.
+     *
+     *  Here is an example of searching for Partners named *ABC* from Belgium and Germany whose language is not english ::
+     *
+     *      [('name','=','ABC'),'!',('language.code','=','en_US'),'|',('country_id.code','=','be'),('country_id.code','=','de'))
+     *
+     *  The '&' is omitted as it is the default, and of course we could have used '!=' for the language, but what this domain really represents is::
+     *
+     *      (name is 'ABC' AND (language is NOT english) AND (country is Belgium OR Germany))
+	 * @param index Index where the specified filter should be added
+	 * @param operator
+	 */
+	public void add(int index, FilterOperator operator){
 		switch (operator) {
 		case OR:
-			filters.add("|");
+			filters.add(index,"|");
 			break;
 		case NOT:
-			filters.add("!");
+			filters.add(index,"!");
 			break;
 		default:
 			break;
@@ -114,5 +148,13 @@ public class FilterCollection {
 	 */
 	public Object[] getFilters(){
 		return filters.toArray(new Object[filters.size()]);
+	}
+	
+	/**
+	 * Returns the number of filters that are configured
+	 * @return
+	 */
+	public int size(){
+		return filters.size();
 	}
 }
